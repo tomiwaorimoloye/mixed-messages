74fdd0f5a12d23f5eec9b368e319e98796a51d14
Tomiwa Orimoloye <gabeorimoloye@gmail.com>
2021-09-21 04:00:37 +0100

    Replace icons with toggle button style & functionality

--- theme.js
+++ theme.js
@@ -1,25 +1,19 @@
-let currentTheme = document.querySelector('button.active').dataset.theme
-let themeButtons = document.querySelectorAll('#theme-toggle > button')
+let toggleContainer = document.querySelector('#toggle-container')
+let currentTheme = toggleContainer.classList[0]
 
-themeButtons.forEach(themeButton => {
-    themeButton.addEventListener('click', setTheme)
-    // themeButton.addEventListener('click', sdk)
-})
+toggleContainer.addEventListener('click', setTheme)
 
 function setTheme(event) {
-    if (!event.target.classList.contains('active')) {
-        event.target.classList.add('active')
-
-        // make sure only one button is active at a time
-        themeButtons.forEach(button => {
-            if (button.classList.contains('active') && button !== event.target) {
-                button.classList.remove('active')
-            }
-        })
+    if (!toggleContainer.classList.contains('light')) {
+        toggleContainer.classList.add('light')
+        toggleContainer.classList.remove('dark')
+    } else {
+        toggleContainer.classList.remove('light')
+        toggleContainer.classList.add('dark')
     }
 
     // update the current theme
-    currentTheme = event.target.dataset.theme
+    currentTheme = toggleContainer.classList[0]
 
     changeStyle()
 }
@@ -31,13 +25,13 @@ function changeStyle() {
         root.style.setProperty('--dark-blue', '#161932')
         root.style.setProperty('--highlight-color', '#77f0f6')
         root.style.setProperty('--text-color', '#d9e3fe')
+        document.body.classList.remove('light')
     } else {
         root.style.setProperty('--background-color', '#e7e9ed')
         root.style.setProperty('--text-color', '#33374b')
         root.style.setProperty('--highlight-color', '#2d539e')
         root.style.setProperty('--dark-blue', 'white')
+        document.body.classList.add('light')
     }
 
 }
-
-
