f4472f97ec2149a43f067457e1f290fd6ccc54bb
Tomiwa Orimoloye <gabeorimoloye@gmail.com>
2021-09-21 02:40:17 +0100

    Code theme switch function

--- theme.js
+++ theme.js
@@ -1,4 +1,4 @@
-let currentTheme = document.querySelector('button.active')
+let currentTheme = document.querySelector('button.active').dataset.theme
 let themeButtons = document.querySelectorAll('#theme-toggle > button')
 
 themeButtons.forEach(themeButton => {
@@ -17,6 +17,27 @@ function setTheme(event) {
             }
         })
     }
+
+    // update the current theme
+    currentTheme = event.target.dataset.theme
+
+    changeStyle()
+}
+
+function changeStyle() {
+    let root = document.querySelector(':root')
+    if (currentTheme === 'dark') {
+        root.style.setProperty('--background-color', '#1f2140')
+        root.style.setProperty('--dark-blue', '#161932')
+        root.style.setProperty('--highlight-color', '#77f0f6')
+        root.style.setProperty('--text-color', '#d9e3fe')
+    } else {
+        root.style.setProperty('--background-color', '#e7e9ed')
+        root.style.setProperty('--text-color', '#33374b')
+        root.style.setProperty('--highlight-color', '#2d539e')
+        root.style.setProperty('--dark-blue', 'white')
+    }
+
 }
 
 
